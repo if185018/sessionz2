1394775ba2d3cf6e553ae3c2752979c46134f540
Iram Fattah <iramfattah94@gmail.com>
2020-03-03 17:04:03 -0500

    install pods for firebase database

---
+++ Sessionz2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sessionz2
+//
+//  Created by C4Q on 3/3/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sessionz2Tests/Sessionz2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Sessionz2Tests.swift
+//  Sessionz2Tests
+//
+//  Created by C4Q on 3/3/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import XCTest
+@testable import Sessionz2
+
+class Sessionz2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
