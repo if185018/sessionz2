2ca568f5e71bebde723f4aaa148a530b32da6d9c
Iram Fattah <iramfattah94@gmail.com>
2020-03-04 11:38:56 -0500

    add custom colors to app

---
+++ Sessionz2/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Sessionz2
+//
+//  Created by C4Q on 3/4/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    //MARK: Properties
+    
+    
+    
+    
+    //MARK: Lifecycle
+    
+    
+    
+    
+}
---
+++ Sessionz2/Utils/ViewController+Extension.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController+Extension.swift
+//  Sessionz2
+//
+//  Created by C4Q on 3/3/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentAlertController(withTitle title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func shouldPresentLoadingView(_ present: Bool, message: String? = nil) {
+        if present {
+            let loadingView = UIView()
+            loadingView.frame = self.view.frame
+            loadingView.backgroundColor = .black
+            loadingView.alpha = 0
+            loadingView.tag = 1
+            
+            let indicator = UIActivityIndicatorView()
+            indicator.style = .whiteLarge
+            indicator.center = view.center
+            
+            let label = UILabel()
+            label.text = message
+            label.font = UIFont.systemFont(ofSize: 20)
+            label.textColor = .white
+            label.textAlignment = .center
+            label.alpha = 0.87
+            
+            view.addSubview(loadingView)
+            loadingView.addSubview(indicator)
+            loadingView.addSubview(label)
+            
+            label.centerX(inView: view)
+            label.anchor(top: indicator.bottomAnchor, paddingTop: 32)
+            
+            indicator.startAnimating()
+            
+            UIView.animate(withDuration: 0.3) {
+                loadingView.alpha = 0.7
+            }
+        } else {
+            view.subviews.forEach { (subview) in
+                if subview.tag == 1 {
+                    UIView.animate(withDuration: 0.3, animations: {
+                        subview.alpha = 0
+                    }, completion: { _ in
+                        subview.removeFromSuperview()
+                    })
+                }
+            }
+        }
+    }
+}
