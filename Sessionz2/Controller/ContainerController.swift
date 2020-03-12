dfb4915ddb7833a6d251fd1f6521807d095ebd22
Iram Fattah <iramfattah94@gmail.com>
2020-03-12 10:57:37 -0400

    added methods to configure container controller

--- Sessionz2/Controller/ContainerController.swift
+++ Sessionz2/Controller/ContainerController.swift
@@ -15,8 +15,17 @@ class ContainerController: UIViewController {
     private var menuController: MenuController!
     private var isExpanded = false
     private lazy var xOrigin = self.view.frame.width - 80
+    private let blackView = UIView()
     
+     private let user: AppUser
     
+    override var prefersStatusBarHidden: Bool {
+        return isExpanded
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return .slide
+    }
     
     //MARK: Lifecycle
     
@@ -24,10 +33,27 @@ class ContainerController: UIViewController {
         super.viewDidLoad()
     }
     
+    //MARK: Init
+    
+    init(user: AppUser) {
+           self.user = user
+           super.init(nibName: nil, bundle: nil)
+       }
+       
+       required init?(coder aDecoder: NSCoder) {
+           fatalError("init(coder:) has not been implemented")
+       }
+    
     
     
     
     //MARK: Helper Methods
+    
+    private func configure() {
+        view.backgroundColor = .backgroundColor
+        configureHomeController()
+        
+    }
     
     private func configureHomeController() {
         addChild(homeController)
@@ -36,12 +62,55 @@ class ContainerController: UIViewController {
         
     }
     
+    private func configureBlackView() {
+        self.blackView.frame = CGRect(x: xOrigin,
+                                      y: 0,
+                                      width: 80,
+                                      height: self.view.frame.height)
+        blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        blackView.alpha = 0
+        view.addSubview(blackView)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissMenu))
+        blackView.addGestureRecognizer(tap)
+    }
     
-    func configureMenuController(with user: AppUser) {
+    
+    private func configureMenuController(with user: AppUser) {
          menuController = MenuController(user: user)
         menuController.didMove(toParent: self)
         view.insertSubview(menuController.view, at: 0)
         
+    }
+    
+    private func animateStatusBar() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+            self.setNeedsStatusBarAppearanceUpdate()
+        }, completion: nil)
+    }
+    
+   private func animateMenu(shouldExpand: Bool, completion: ((Bool) -> Void)? = nil) {
+        if shouldExpand {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                self.homeController.view.frame.origin.x = self.xOrigin
+                self.blackView.alpha = 1
+            }, completion: nil)
+        } else {
+            self.blackView.alpha = 0
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                self.homeController.view.frame.origin.x = 0
+            }, completion: completion)
+        }
+        
+        animateStatusBar()
+    }
+    
+    
+    //MARK: Selectors
+    
+    @objc func dismissMenu() {
+        isExpanded = false
+        animateMenu(shouldExpand: isExpanded)
     }
     
 }
