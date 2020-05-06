ea2e04e69973e6872ebc94d6140eb959c5a9c064
Iram Fattah <iramfattah94@gmail.com>
2020-05-06 11:26:57 -0400

    add user profile vc

---
+++ Sessionz2/Controller/UserProfileVC.swift
@@ -0,0 +1,31 @@
+//
+//  UserProfileVC.swift
+//  Sessionz2
+//
+//  Created by C4Q on 5/6/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class UserProfileVC: UITableViewController {
+    
+    
+    //MARK: Properties
+    var user: AppUser?
+    
+    
+    
+    
+    //MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: Firebase API
+    
+    
+}
