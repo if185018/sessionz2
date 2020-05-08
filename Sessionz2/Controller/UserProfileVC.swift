521b2f8c7718f427982b336c3eae5cb78e9529d8
Iram Fattah <iramfattah94@gmail.com>
2020-05-08 11:36:03 -0400

    add user profile header

--- Sessionz2/Controller/UserProfileVC.swift
+++ Sessionz2/Controller/UserProfileVC.swift
@@ -44,4 +44,7 @@ class UserProfileVC: UITableViewController {
             
         
     }
+    
+    
+    
 }
---
+++ Sessionz2/Views/UserProfileHeader.swift
@@ -0,0 +1,14 @@
+//
+//  UserProfileHeader.swift
+//  Sessionz2
+//
+//  Created by C4Q on 5/8/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileHeader: UITableViewCell {
+
+
+}
