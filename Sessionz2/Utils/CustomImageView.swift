37c13ba39630093ef84499044d77ded1df825067
Iram Fattah <iramfattah94@gmail.com>
2020-05-26 12:54:58 -0400

    implement custom image view with cache

--- Sessionz2/Utils/CustomImageView.swift
+++ Sessionz2/Utils/CustomImageView.swift
@@ -6,4 +6,54 @@
 //  Copyright © 2020 Iram Fattah. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+var imageCache = [String: UIImage]()
+
+class CustomImageView: UIImageView {
+    
+    var lastImgUrlUsedToLoadImage: String?
+    
+    
+    func loadImage(with urlString: String) {
+        self.image = nil
+        
+        lastImgUrlUsedToLoadImage = urlString
+        
+        
+        // check if image exists in cache
+        if let cachedImage = imageCache[urlString] {
+            self.image = cachedImage
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+        
+        // handle error
+        if let error = error {
+            print("Failed to load image with error", error.localizedDescription)
+        }
+        
+            if self.lastImgUrlUsedToLoadImage != url.absoluteString {
+                return
+            }
+            
+            // image data
+            guard let imageData = data else { return }
+            
+            // create image using image data
+            let photoImage = UIImage(data: imageData)
+            
+            // set key and value for image cache
+            imageCache[url.absoluteString] = photoImage
+            
+            // set image
+            DispatchQueue.main.async {
+                self.image = photoImage
+            }
+        }.resume()
+    }
+    
+}
