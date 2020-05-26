1d39da3465ec1aae8b117863721ad346cf0df61d
Iram Fattah <iramfattah94@gmail.com>
2020-05-26 12:53:25 -0400

    add edit profile VC

---
+++ Sessionz2/Controller/EditProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EditProfileViewController.swift
+//  Sessionz2
+//
+//  Created by C4Q on 5/25/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
---
+++ Sessionz2/Utils/CustomImageView.swift
@@ -0,0 +1,9 @@
+//
+//  CustomImageView.swift
+//  Sessionz2
+//
+//  Created by C4Q on 5/26/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import Foundation
