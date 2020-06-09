66e1d3ef662d28000c9ab894bf881e39f77f195d
Iram Fattah <iramfattah94@gmail.com>
2020-06-09 12:25:56 -0400

    add new message model

---
+++ Sessionz2/Models/Message.swift
@@ -0,0 +1,25 @@
+//
+//  Message.swift
+//  Sessionz2
+//
+//  Created by C4Q on 6/9/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Message {
+    var messageText: String!
+    var fromId: String!
+    var toId: String!
+    var creationDate: Date!
+    var imageUrl: String?
+    var imageHeight: NSNumber?
+    var imageWidth: NSNumber?
+    var videoUrl: String?
+    var read: Bool!
+    
+    
+    
+}
