cab6f25324a8c3d8f9f0e02b35359a4da374d2bf
Iram Fattah <iramfattah94@gmail.com>
2020-06-11 13:21:00 -0400

    add chat controller

---
+++ Sessionz2/Controller/ChatController.swift
@@ -0,0 +1,29 @@
+//
+//  ChatController.swift
+//  Sessionz2
+//
+//  Created by C4Q on 6/11/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import MobileCoreServices
+import AVFoundation
+
+private let reuseIdentifier = "ChatCell"
+
+class ChatController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
