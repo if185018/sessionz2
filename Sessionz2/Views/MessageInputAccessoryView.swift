448a2bf61f59348b36bbcd003587c7b288f66386
Iram Fattah <iramfattah94@gmail.com>
2020-06-15 12:06:41 -0400

    add constraints for text view accessory view

--- Sessionz2/Views/MessageInputAccessoryView.swift
+++ Sessionz2/Views/MessageInputAccessoryView.swift
@@ -41,6 +41,37 @@ class MessageInputAccessoryView: UIView {
         return iv
     }()
     
+    //MARK: Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        autoresizingMask = .flexibleHeight
+        
+        backgroundColor = .white
+        
+        addSubview(sendButton)
+        sendButton.anchor(top: topAnchor, left: nil, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 8, width: 50, height: 50)
+        
+        addSubview(uploadImageIcon)
+        uploadImageIcon.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectImage)))
+        uploadImageIcon.isUserInteractionEnabled = true
+        uploadImageIcon.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 4, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 44, height: 44)
+        
+        addSubview(messageInputTextView)
+        messageInputTextView.anchor(top: topAnchor, left: uploadImageIcon.rightAnchor, bottom: safeAreaLayoutGuide.bottomAnchor, right: sendButton.leftAnchor, paddingTop: 8, paddingLeft: 4, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .lightGray
+        addSubview(separatorView)
+        separatorView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     
     
