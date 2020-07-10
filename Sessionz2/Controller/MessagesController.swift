dba771b541ff1dc8465100ad3136c8dab1c067fb
Iram Fattah <iramfattah94@gmail.com>
2020-07-10 14:01:35 -0400

    add messages vc

---
+++ Sessionz2/Controller/MessagesController.swift
@@ -0,0 +1,27 @@
+//
+//  MessagesController.swift
+//  Sessionz2
+//
+//  Created by C4Q on 7/10/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+private let reuseIdentifier = "MessagesCell"
+
+
+class MessagesController: UITableViewController {
+    
+    //MARK: Properties
+    
+    var messages = [Message]()
+    var messagesDictionary = [String: Message]()
+    
+    
+    
+    
+    
+}
