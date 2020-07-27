bcc2df15fe8a8aca28c2267ee6c030019f8a455a
Iram Fattah <iramfattah94@gmail.com>
2020-07-27 14:45:56 -0400

    new message controller added

---
+++ Sessionz2/Controller/NewMessageController.swift
@@ -0,0 +1,19 @@
+//
+//  NewMessageController.swift
+//  Sessionz2
+//
+//  Created by C4Q on 7/27/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+private let reuseIdentifier = "NewMessageCell"
+
+class NewMessageController: UITableViewController {
+
+    // MARK: - Properties
+    
+    
+}
