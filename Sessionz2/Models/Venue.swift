11f6283277acc3223426949b78112b7154aec201
Iram Fattah <iramfattah94@gmail.com>
2020-07-29 18:05:06 -0400

    add venue model

---
+++ Sessionz2/Models/Venue.swift
@@ -0,0 +1,14 @@
+//
+//  Venue.swift
+//  Sessionz2
+//
+//  Created by C4Q on 7/29/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import Foundation
+
+struct Venue {
+    var name: String
+    
+}
