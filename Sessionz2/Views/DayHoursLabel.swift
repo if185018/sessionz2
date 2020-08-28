4ad462d4cba846dbfd0a47646f154ca145ddb0ca
Iram Fattah <iramfattah94@gmail.com>
2020-08-27 23:12:45 -0400

    hours of operation and day label

---
+++ Sessionz2/Views/DayHoursLabel.swift
@@ -0,0 +1,14 @@
+//
+//  DayHoursLabel.swift
+//  Sessionz2
+//
+//  Created by C4Q on 8/27/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+
+class DayHoursLabel: UILabel {
+    
+}
---
+++ Sessionz2/Views/HoursOfOperationView.swift
@@ -0,0 +1,15 @@
+//
+//  HoursOfOperationView.swift
+//  Sessionz2
+//
+//  Created by C4Q on 8/27/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class HoursOfOperationView: UIView {
+
+    
+    
+}
