abcb7e228a8f78ff1c3ecd565b7775a2075cf988
Iram Fattah <iramfattah94@gmail.com>
2020-08-29 00:09:24 -0400

    Hours Of Operation begin

---
+++ Sessionz2/Models/HoursOfOperation.swift
@@ -0,0 +1,25 @@
+//
+//  HoursOfOperation.swift
+//  Sessionz2
+//
+//  Created by C4Q on 8/28/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import Foundation
+
+struct HoursOfOperation {
+    func figureItOut() {
+        let calendar = Calendar.current
+        let date = Date()
+        let dateInterval = calendar.dateInterval(of: .hour, for: date)
+        
+        let dateComponents = DateComponents(hour: 8)
+        
+    }
+    
+    
+    
+    
+    
+}
