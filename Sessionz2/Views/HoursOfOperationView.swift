b021e82f39c8f8fa084abae31b91acace31e5d76
Iram Fattah <iramfattah94@gmail.com>
2020-09-08 17:27:32 -0400

    add labels for days

--- Sessionz2/Views/HoursOfOperationView.swift
+++ Sessionz2/Views/HoursOfOperationView.swift
@@ -10,6 +10,69 @@ import UIKit
 
 class HoursOfOperationView: UIView {
 
+    var hoursOfOperation: [HoursOfOperation]?
+    
+    
+    
+    private lazy var sundayLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textAlignment = .center
+            return label
+    }()
+    
+    private lazy var mondayLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textAlignment = .center
+            return label
+    }()
+    
+    private lazy var tuesdayLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textAlignment = .center
+            return label
+    }()
+    
+    private lazy var wednesdayLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textAlignment = .center
+            return label
+    }()
+    
+    private lazy var thursdayLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textAlignment = .center
+            return label
+    }()
+    
+    private lazy var fridayLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textAlignment = .center
+            return label
+    }()
+    
+    private lazy var saturdayLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textAlignment = .center
+            return label
+    }()
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     
 }
