09b92b0c98393691b641ffc8238babef35107b4c
Iram Fattah <iramfattah94@gmail.com>
2020-09-09 16:35:48 -0400

    fill labels in stack view

--- Sessionz2/Views/HoursOfOperationView.swift
+++ Sessionz2/Views/HoursOfOperationView.swift
@@ -67,12 +67,34 @@ class HoursOfOperationView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupLabelsWithHours()
+        
+       
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    private func setupLabelsWithHours() {
+         let labels = [sundayLabel, mondayLabel, tuesdayLabel, wednesdayLabel,thursdayLabel,fridayLabel,saturdayLabel]
+        
+        guard let hoursOfOperation = self.hoursOfOperation else {return}
+        
+        let stackView = UIStackView(arrangedSubviews: labels)
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5
+        
+        stackView.anchor(top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingTop: 4, paddingLeft: 4, paddingBottom: 4, paddingRight: 4)
+        
+        for num in 0..<hoursOfOperation.count {
+            labels[num].text = hoursOfOperation[num].generateDisplayText()
+        }
+        
+    }
     
     
 }
