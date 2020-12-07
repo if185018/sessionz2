9acaa23ad975abeadccc5d6c77e291697556228a
Iram Fattah <iramfattah94@gmail.com>
2020-12-07 09:25:29 -0500

    Add challenge sort models

---
+++ Sessionz2/Controller/ChallengeSortingVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChallengeSortingVC.swift
+//  Sessionz2
+//
+//  Created by C4Q on 12/7/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeSortingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
---
+++ Sessionz2/Models/ChallengeSort.swift
@@ -0,0 +1,14 @@
+//
+//  ChallengeSort.swift
+//  Sessionz2
+//
+//  Created by C4Q on 12/7/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+
+import UIKit
+struct ChallengeSort {
+    let title: String
+    let icon: UIImage
+}
